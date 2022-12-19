a59782136c3d3e4417feda4721e10ac1a2d6d41b
Samuel Olamide <sam99kupo@gmail.com>
2022-12-19 04:16:44 +0100

    WIP: tests for contract

---
+++ test/Project.test.js
@@ -0,0 +1,56 @@
+const assert = require("assert");
+const ganache = require("ganache");
+const options = {};
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+
+
+const compiledGeneratorContract = require('../eth/build/Generator.json')
+const compiledProjectContract = require('../eth/build/Project.json')
+
+let accounts;
+let generator;
+let project;
+let projectAddress;
+
+
+
+beforeEach(async() => {
+    accounts = await web3.eth.getAccounts();
+
+  try {
+    generator = await new web3.eth.Contract(compiledGeneratorContract.abi)
+    .deploy({
+        data: compiledGeneratorContract.evm.bytecode.object,
+    })
+    .send({
+        from: accounts[0],
+        gas:'2000000'
+    })
+
+    await generator.methods.createProject("100").send({
+        from: accounts[0],
+        gas:"2000000"
+    });
+
+    const projectAddresseses = await generator.methods.getDeployedProjects().call();
+    projectAddress = projectAddresseses[0];
+
+    project = await new web3.eth.Contract(compiledProjectContract.abi, projectAddress);
+
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+})
+
+
+describe("Project Tests", () => {
+    it("Deploys the project generator", () => {
+        assert.ok(generator.options.address);
+    });
+
+    it("Deploys the project contract", () => {
+        assert.ok(project.options.address);
+    });
+})
